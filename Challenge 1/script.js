7d17dac200443dcefc3f1a85846cc3025ed990bc
Hilal Fakherdine <hilalfeld@gmail.com>
2023-09-03 00:06:45 +0300

    Challenge 1 - Done with Step 4

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -15,3 +15,32 @@ lastName = "Feld2";
 console.log(age);
 console.log(lastName);
 console.log(DOB);
+
+console.log(typeof "This is a text");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1215);
+console.log(typeof "999");
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof undefined);
+
+var carName = "Ford";
+var newModel = true;
+var plateNum = 12345678;
+var seats = ["Front seats", "Back seats"];
+var nationality = "British";
+var owner = {
+  name: "Henry",
+  age: 25,
+  carLover: undefined,
+  license: null,
+};
+
+console.log(carName, typeof carName);
+console.log(newModel, typeof newModel);
+console.log(plateNum, typeof plateNum);
+console.log(seats, typeof seats);
+console.log(nationality, typeof nationality);
+console.log(owner, typeof owner);
