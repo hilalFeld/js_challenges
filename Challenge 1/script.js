44b52ea5dd290b04f86656423609b292cecf64da
Hilal Fakherdine <hilalfeld@gmail.com>
2023-09-03 00:24:58 +0300

    Challenge 1 - Done with Step 5

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -44,3 +44,36 @@ console.log(plateNum, typeof plateNum);
 console.log(seats, typeof seats);
 console.log(nationality, typeof nationality);
 console.log(owner, typeof owner);
+
+var myText = "Hello World!";
+var myText1 = "Hello World!";
+var myText2 = `Hello World!`;
+
+const counter = 3;
+const backtick = `This is my text using backticks ${counter} times`;
+const singleQuote = "This is my text using single quotations ${counter} times";
+const doubleQuote = "This is my text using double quotations ${counter} times";
+const tryThisAlso = `This is my new version, it’s the ${2 + 3}th version`;
+
+console.log(counter);
+console.log(backtick);
+console.log(singleQuote);
+console.log(doubleQuote);
+console.log(tryThisAlso);
+
+console.log("The (counter) variable is of type number.");
+console.log(
+  "The (backtick) variable uses the backticks to be able to add the value of\
+   the (counter) variable inside it's string using it's notation ${counter}"
+);
+
+console.log(
+  "Both the (singleQuote) and (doubleQuote) variables cannot log the value of the (counter) variable,\
+   since for this notation to work, ${counter}, the backticks should be used. However, there is no difference\
+    between using any of them, it is back to the developers to choose what suits them better."
+);
+
+console.log(
+  "The (tryThisAlso) variable uses the backticks. Instead of ${counter},\
+   a ${2 + 3} code used, where it's result is 5 on the console as you see."
+);
