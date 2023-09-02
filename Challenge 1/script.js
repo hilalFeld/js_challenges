b3028d59df2e2c7e687d55d60e81a9bbfeaff012
Hilal Fakherdine <hilalfeld@gmail.com>
2023-09-02 23:51:24 +0300

    Challenge 1 - Done with Step 3

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -1,2 +1,17 @@
 var name = "Hilal";
 alert(name);
+
+var age = 22;
+let lastName = "Feld";
+const DOB = 2000;
+
+console.log(age);
+console.log(lastName);
+console.log(DOB);
+
+age = 23;
+lastName = "Feld2";
+
+console.log(age);
+console.log(lastName);
+console.log(DOB);
